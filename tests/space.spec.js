6b2f2b0be5189b35e80ecb3ee47b9ea8659e7efd
SergeInGithub <nyiriserge@gmail.com>
2023-10-20 16:09:26 +0200

    ft: add circle ci

--- tests/space.spec.js
+++ tests/space.spec.js
@@ -14,6 +14,7 @@ describe('Space test suite', () => {
     ]);
     expect(response.body).toHaveLength(6);
     expect(response.statusCode).toBe(200);
+
     //* Testing a single element in the array
     expect(response.body).toEqual(expect.arrayContaining(['Earth']));
   });
